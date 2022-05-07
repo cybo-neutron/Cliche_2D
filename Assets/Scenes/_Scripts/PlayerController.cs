73b4783d86fce302d986525440068d9b4330dd24
cybo-neutron <cyboneutron@gmail.com>
2022-05-07 10:45:00 +0530

    random changes

--- Assets/Scenes/_Scripts/PlayerController.cs
+++ Assets/Scenes/_Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-ausing System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,14 +7,32 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed;
     Rigidbody2D rb;
 
+    [Header("Jump")]
+    [SerializeField] float jumpVelocity;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float fallingGravity = 1f;
+    [SerializeField] float normalGravity = 1f;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = normalGravity;
     }
 
     void Update()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetAxisRaw("Jump") > 0)
+        {
+            Jump();
+        }
         rb.velocity = new Vector2(horizontalInput * speed * Time.deltaTime, rb.velocity.y);
     }
+
+    void Jump()
+    {
+
+    }
+
 }
